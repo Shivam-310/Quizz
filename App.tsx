d679b46b9e1421cf5cc7025c4a1d379cc5524306
Shivam <shivamchaudhary310899@gmail.com>
2024-12-15 16:50:52 +0530

    installing dependencies

--- App.tsx
+++ App.tsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native';
 import React from 'react';
+import Home from './screens/home';
+import Quiz from './screens/quiz';
+import Result from './screens/result';
+import { NavigationContainer } from '@react-navigation/native';
+import MyStack from './navigation';
 
 const App = () => {
   return (
-    <View>
-      <Text>App</Text>
+    <View style={styles.container}>
+      <NavigationContainer>
+        <MyStack />
+      </NavigationContainer>
     </View>
   );
 };
 
 export default App;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container:{
+    paddingTop: 40,
+    paddingHorizontal: 16,
+  },
+});
