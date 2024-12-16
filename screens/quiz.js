ef55a5a2faa9a7b0caf1a3933cc6578946f10753
Shivam <shivamchaudhary310899@gmail.com>
2024-12-16 11:51:23 +0530

    designing buttons

--- screens/quiz.js
+++ screens/quiz.js
@@ -5,24 +5,24 @@ const Quiz = ({navigation}) => {
   return (
     <View style={styles.container}>
       <View style={styles.top}>
-        <Text>This is a question</Text>
+        <Text style={styles.question}>Q. This is a question</Text>
       </View>
       <View style={styles.options}>
-        <TouchableOpacity><Text>Option 1</Text></TouchableOpacity>
-        <TouchableOpacity><Text>Option 1</Text></TouchableOpacity>
-        <TouchableOpacity><Text>Option 1</Text></TouchableOpacity>
-        <TouchableOpacity><Text>Option 1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}><Text style={styles.option}>Option 1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}><Text style={styles.option}>Option 1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}><Text style={styles.option}>Option 1</Text></TouchableOpacity>
+        <TouchableOpacity style={styles.optionButton}><Text style={styles.option}>Option 1</Text></TouchableOpacity>
       </View>
       <View style={styles.bottom}>
-        <TouchableOpacity>
-          <Text>SKIP</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>SKIP</Text>
         </TouchableOpacity>
-        <TouchableOpacity>
-          <Text>NEXT</Text>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>NEXT</Text>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate('Result')}>
+        {/* <TouchableOpacity onPress={() => navigation.navigate('Result')}>
             <Text>END</Text>
-        </TouchableOpacity>
+        </TouchableOpacity> */}
       </View>
     </View>
   );
@@ -32,7 +32,8 @@ export default Quiz;
 
 const styles = StyleSheet.create({
   container:{
-    padding: 12,
+    paddingTop: 40,
+    paddingHorizontal: 20,
     height: '100%',
   },
   top:{
@@ -47,5 +48,33 @@ const styles = StyleSheet.create({
     paddingVertical: 16,
     justifyContent: 'space-between',
     flexDirection: 'row',
+  },
+  button:{
+    backgroundColor: '#1A759F',
+    padding: 12,
+    paddingHorizontal: 16,
+    borderRadius: 16,
+    alignItems: 'center',
+    marginBottom: 30,
+  },
+  buttonText:{
+    fontSize: 18,
+    fontWeight: '600',
+    color: 'white',
+  },
+  question:{
+    fontSize: 28,
+  },
+  option:{
+    fontSize: 18,
+    fontWeight: '500',
+    color: 'white',
+  },
+  optionButton:{
+    paddingVertical: 12,
+    paddingHorizontal: 12,
+    marginVertical: 6,
+    backgroundColor: '#34A0A4',
+    borderRadius: 12,
   },
 });
